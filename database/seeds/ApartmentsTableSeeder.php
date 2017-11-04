0e781ec29010d42acf615f9d83c7e2356b6ebdcc
Vitaliy Skladanovskiy <j3ff3.me@gmail.com>
2017-11-04 23:29:01 +0200

    init

--- database/seeds/ApartmentsTableSeeder.php
+++ database/seeds/ApartmentsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+use App\Apartments;
 
 class ApartmentsTableSeeder extends Seeder
 {
@@ -12,7 +14,23 @@ class ApartmentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $content = Storage::disk('local')->get('other/property-data.csv');
+
+        $lines = explode(PHP_EOL, $content);
+
+        for ($i = 1; $i < count($lines); $i++) {
+            $fields = explode(',', $lines[$i]);
+
+            $apartment = new Apartments;
+            $apartment->name = $fields[0];
+            $apartment->price = $fields[1];
+            $apartment->bedrooms = $fields[2];
+            $apartment->bathrooms = $fields[3];
+            $apartment->storeys = $fields[4];
+            $apartment->garages = $fields[5];
+
+            $apartment->save();
+        }
     }
 
 }
